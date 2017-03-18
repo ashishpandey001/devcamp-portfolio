e7c4ac5aa795aa095e502a6d943eb778102544d2
Ashish Pandey <hmaster0@gmail.com>
2017-03-18 07:39:20 +0530

    Built pages via controller generator

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,3 +1,4 @@
+# RESTful Controller for managing blogs
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
@@ -58,13 +59,14 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
