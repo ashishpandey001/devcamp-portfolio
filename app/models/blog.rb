b5c2fdc4d0296b7d7cc0d7bbce8937a33b00fc19
Ashish Pandey <hmaster0@gmail.com>
2017-03-20 04:58:12 +0530

    Integrated friendly routes for blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,5 @@
+# Blog model to interact with the blogs table
 class Blog < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
---
+++ db/migrate/20170319224039_add_slug_to_blogs.rb
@@ -0,0 +1,7 @@
+# Add unique slug field to blogs table
+class AddSlugToBlogs < ActiveRecord::Migration[5.0]
+  def change
+    add_column :blogs, :slug, :string
+    add_index :blogs, :slug, unique: true
+  end
+end
