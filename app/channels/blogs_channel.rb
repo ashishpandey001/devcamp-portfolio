69598b89b23945109255f2ca1b1e3a7128eb0b39
Ashish Pandey <hmaster0@gmail.com>
2017-04-06 17:39:46 +0530

    Implemented actioncable for comments

---
+++ app/channels/blogs_channel.rb
@@ -0,0 +1,14 @@
+# Channel to handle Comment broadcasts
+class BlogsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "blogs_#{params['blog_id']}_channel"
+  end
+
+  def unsubscribed
+    # code
+  end
+
+  def send_comment(data)
+    current_user.comments.create!(content: data['comment'], blog_id: data['blog_id'])
+  end
+end
