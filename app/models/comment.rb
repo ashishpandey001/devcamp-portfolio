b2fa583caf170c8d8f08b51060a49d0091cd43e4
Ashish Pandey <hmaster0@gmail.com>
2017-04-06 14:08:56 +0530

    Refactored comment form into a partial and added validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
+# Model to manage the comments table
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
+  validates :content, presence: true, length: { minimum: 5, maximum: 100 }
 end
